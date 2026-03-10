65d392302665413bda2370b084e4780a9aaadab2
nakibworkspace <rajnakib2003@gmail.com>
2026-03-10 20:17:48 +0000

    added dbt simulation on readme file

---
+++ project1/dbt_project/models/marts/mart_weekend_vs_weekday.sql
@@ -0,0 +1,13 @@
+select
+    case
+        when pickup_day_of_week in (6, 7) then 'Weekend'
+        else 'Weekday'
+    end                                      as day_type,
+    count(*)                                 as total_trips,
+    round(avg(fare_amount), 2)               as avg_fare,
+    round(avg(tip_amount), 2)                as avg_tip,
+    round(avg(trip_distance), 2)             as avg_distance,
+    round(avg(trip_duration_minutes), 2)     as avg_duration_minutes
+from {{ ref('int_taxi_trips_enriched') }}
+group by day_type
+order by day_type
